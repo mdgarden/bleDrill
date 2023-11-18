d528126640f01fecc9d94d1204346b102fdf4c37
Madang <chooi9522@gmail.com>
2023-11-18 15:18:04 +0900

    screen 추가

---
+++ src/screens/BLEScreen.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export function BLEScreen() {
+  return (
+    <View>
+      <Text>Logs</Text>
+    </View>
+  );
+}
---
+++ src/screens/ScanningScreen.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export function ScanningScreen() {
+  return (
+    <View>
+      <Text>Scanning</Text>
+    </View>
+  );
+}
